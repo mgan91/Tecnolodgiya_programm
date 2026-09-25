f13931a04ea161b7eb2950e4eb351c4a12d1850e
mganzvind <ganzvind01@mail.ru>
2026-09-25 09:47:15 +0700

    Изменил приветствие на 'драсте антон'

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace MyNamespace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("драсте антон");
         }
     }
 };
